0200da147e8a1718b4d713b7f91d33c63dde089c
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2022-10-22 17:32:17 +0300

    field generation modified, materials added

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class field : MonoBehaviour
+public class Field : MonoBehaviour
 {
     public GameObject template;
-    public int room_x = 16;
-    public int room_z = 16;
+    public int room_x;
+    public int room_z;
 
-    void Start()
+    void RectangleGenerator()
     {
         List<List<GameObject>> plates = new List<List<GameObject>>();
         float sx = template.transform.localScale.x;
         float sz = template.transform.localScale.z;
         Vector3 v = new Vector3(0, 0, 0);
+        room_x = Random.Range(7, 15);
+        room_z = Random.Range(7, 15);
         for (int k = 0; k < room_z; k++){
             v.z = k*sz;
             plates.Add(new List<GameObject>());
@@ -23,7 +25,23 @@ public class field : MonoBehaviour
                 plates[k][i].transform.position = v;
             }
         }
+    }
 
+    void OtherShapeGenerator()
+    {
+        
+    }
+
+    void Start()
+    {
+        // field type
+        Debug.Log("asfsfgdgh");
+        int fieldType = Random.Range(0, 1);
+        if (!(fieldType > 0))
+        {
+            RectangleGenerator();
+        }
+        else { OtherShapeGenerator(); }
     }
 
     // Update is called once per frame
